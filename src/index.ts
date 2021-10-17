57cee7838ef5e4a18794b191d17d0d264f415b87
childhooooo <yumenaut@gmail.com>
2021-10-17 18:09:39 +0900

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export { default as Wrap } from 'components/layout/Wrap';
+export { default as Stacked } from 'components/layout/Stacked';
+export { default as Lined } from 'components/layout/Lined';
