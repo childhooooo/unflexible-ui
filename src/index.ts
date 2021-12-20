9360ff45b8458430531802f3384a83638fc6e8bd
Childhooooo <yumenaut@gmail.com>
2021-12-20 17:21:11 +0900

    Export Columns

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { default as Wrap } from 'components/layout/Wrap';
 export { default as Stacked } from 'components/layout/Stacked';
 export { default as Page } from 'components/layout/Page';
 export { default as Block } from 'components/layout/Block';
+export { default as Columns } from 'components/layout/Columns';
 
 export { default as Figure } from 'components/element/Figure';
 export { default as PlainText } from 'components/element/PlainText';
